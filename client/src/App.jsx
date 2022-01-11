ca6921dd0f9fb8c195c05a4d564c8f7e6aba8532
AZC <albertzcheng@gmail.com>
2022-01-10 23:05:32 -0800

    troubleshooting TailwindCSS not working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-
 const App = () => {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
     </div>
